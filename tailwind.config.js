73afb7e32e0074f1ac42e552f8dfc00f77d0b82f
Modernizando <tulioigortravaioli@gmail.com>
2026-08-31 10:36:05 -0300

    Fix Tailwind scan paths for root React files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   darkMode: "class",
   theme: {
     extend: {},
